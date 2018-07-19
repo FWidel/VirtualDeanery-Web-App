50ea922724319ce409bace4e5ab642d2df992819
FWidel <widelfranciszek@gmail.com>
2018-07-19 17:25:33 +0200

    Modified form, added new input form

--- Deanery/ClientApp/components/Home.tsx
+++ Deanery/ClientApp/components/Home.tsx
@@ -6,24 +6,34 @@ export class Home extends React.Component<RouteComponentProps<{}>, User> {
         super(props);
     }
     userRegister(z: any) {
-        var dataFromForm;
-        dataFromForm = z.target.parentElement.parentElement;
-        console.log(dataFromForm);
+        var formData;
+        formData = z.target.parentElement.parentElement;
+        //console.log(formData);
 
         var request = new XMLHttpRequest();
-        var login = dataFromForm.login.value;
-        var password = dataFromForm.password.value;
-        var email = dataFromForm.email.value;
+        var firstname = formData.firstname.value;
+        var surname = formData.surname.value;
+        var lastname = formData.lastname.value;
+        var password = formData.password.value;
+        var email = formData.email.value;
+        var phone = formData.phone.value;
 
         request.open('POST', serverUri +'user/register', true);
         request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-        dataFromForm.reset();
+        formData.reset();
 
         request.onload = () => {
             alert(request.responseText);
         }
 
-        request.send({ "Login": login, "Password": password, "Email": email });
+        request.send(JSON.stringify({
+            "Firstname": firstname,
+            "Password": password,
+            "Email": email,
+            "Phone": phone,
+            "Lastname": lastname,
+            "Surname": surname
+        }));
     }
 
     public render() {
@@ -33,12 +43,18 @@ export class Home extends React.Component<RouteComponentProps<{}>, User> {
                     <h1>Register</h1>
                     <p>Please fill in this form to create an account.</p>
                     <hr />
-                    <label><b>Login</b></label>
-                    <input type="text" placeholder="Enter Login" id="login" name="login" required />
+                    <label><b>Firstname</b></label>
+                    <input type="text" placeholder="Enter name" id="firstname" name="firstname" required />
+                    <label><b>Lastname</b></label>
+                    <input type="text" placeholder="Enter lastname" id="lastname" name="lastname" required />
+                    <label><b>Surname</b></label>
+                    <input type="text" placeholder="Enter surname" id="surname" name="surname" required />
                     <label><b>Email</b></label>
-                    <input type="text" placeholder="Enter Email" id="email" name="email" required />
+                    <input type="text" placeholder="Enter email" id="email" name="email" required />
+                    <label><b>Phone</b></label>
+                    <input type="text" placeholder="Enter phone number" id="phone" name="phone" required />
                     <label><b>Password</b></label>
-                    <input type="password" placeholder="Enter Password" id="password" name="psw" required />
+                    <input type="password" placeholder="Enter password" id="password" name="psw" required />
                     <hr />
                     <button type="button" onClick={this.userRegister} className="btn btn-default" >Submit</button>
                 </div>
