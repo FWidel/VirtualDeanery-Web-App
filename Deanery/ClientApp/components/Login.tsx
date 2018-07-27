b8aa24a3ac56cb43ba5470f1b3e4cb4a3fcaf3c7
FWidel <widelfranciszek@gmail.com>
2018-07-27 20:26:44 +0200

    Add login page and shall all students

---
+++ Deanery/ClientApp/components/Login.tsx
@@ -0,0 +1,62 @@
+﻿import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+const serverUri = "/api/";
+
+export class Login extends React.Component<RouteComponentProps<{}>, User> {
+    constructor(props: any) {
+        super(props);
+    }
+    userRegister(z: any) {
+        var formData;
+        formData = z.target.parentElement.parentElement;
+        //console.log(formData);
+
+        var request = new XMLHttpRequest();
+        var login = formData.login.value;
+        var password = formData.password.value;
+    
+
+        request.open('POST', serverUri + 'user/register', true);
+        request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+        formData.reset();
+
+        request.onload = () => {
+            alert(request.responseText);
+        }
+
+        request.send(JSON.stringify({
+            "Password": password,
+            "Login" : login
+        }));
+    }
+
+    public render() {
+        return <div>
+            <form className="form-group">
+                <div className="container">
+                    <h1>Login</h1>
+                    <hr />
+                    <label><b>Login</b></label>
+                    <input type="text" placeholder="login" className="form-control" id="login" name="login" required />
+                    <label><b>Password</b></label>
+                    <input type="password" placeholder="password" className="form-control" id="password" name="password" required />
+             
+                    <hr />
+                    <button type="button" onClick={this.userRegister} className="btn btn-default" >Log in</button>
+                </div>
+
+                <div className="container signin">
+                    <p>Dont have account?  <a href="/register">Sign up</a>.</p>
+                </div>
+            </form>
+        </div>;
+    }
+}
+
+
+
+interface User {
+    Login: string,
+    Password: string
+}
+
--- Deanery/Controllers/StudentAllData.cs
+++ Deanery/Controllers/StudentAllData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Deanery.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -11,10 +12,16 @@ namespace Deanery.Controllers
     
     public class StudentAllData : Controller
     {
+        private DbDeaneryContext db = new DbDeaneryContext();
+
+        
+
         [Route("api/student/get-all")]
         public IActionResult Index()
         {
-            return Ok("[ { \"name\" : \"franek\" } ]");
+            var students = db.Student.Where(c => true);
+
+            return Ok(students);
         }
     }
 }
