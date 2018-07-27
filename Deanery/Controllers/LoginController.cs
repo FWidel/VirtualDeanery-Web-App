d9f1341bf82d1286abc959c02d029d8af677e85d
kamil <kamil@DESKTOP-I3BD7EU>
2018-07-27 20:42:29 +0200

    login/pass

---
+++ Deanery/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Deanery.Entities;
+using Deanery.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace Deanery.Controllers
+{
+    public class LoginController : Controller
+    {
+        private DbDeaneryContext db = new DbDeaneryContext();
+        [Route("api/user/login")]
+        // [HttpPost]
+        public IActionResult LoginMethod()
+        {
+            string id = "Jakis", password = "JD";
+
+            var login = db.Student.Where(p => p.Lastname == id&& p.Password == password);
+            if (login.Count()!=0 )       
+             return Ok("Good");          
+            else
+             return Ok("Bad");
+        }
+
+
+    }
+}
