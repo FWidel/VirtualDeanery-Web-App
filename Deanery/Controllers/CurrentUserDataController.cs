19481360cddade47db516a9483f57be3cc3893b9
Arek Pichurski <arekpichurski@gmail.com>
2018-08-04 18:55:08 +0200

    CurrentDatauser

---
+++ Deanery/Controllers/CurrentUserDataController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Deanery.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Deanery.Controllers
+{
+    public class CurrentUserDataController : Controller
+    {
+        private DbDeaneryContext db = new DbDeaneryContext();
+        [Route("api/user/get-user-data")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //HttpContext.Session.SetString("Login", "The Doctor");
+            
+            var login = HttpContext.Session.GetString("Login");
+            var currentUser = db.Student.Where(c => c.Login ==login);
+           
+            return Ok(currentUser);
+        }
+    }
+}
