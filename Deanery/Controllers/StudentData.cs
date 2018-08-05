56b42134e37a226f38dbb33575c6b26ed73dda77
kamil <kamil@DESKTOP-I3BD7EU>
2018-08-05 14:36:36 +0200

    add change/phone/password/firstname

--- Deanery/Controllers/StudentData.cs
+++ Deanery/Controllers/StudentData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Deanery.Entities;
 using Microsoft.AspNetCore.Mvc;
@@ -18,29 +19,37 @@ namespace Deanery.Controllers
         public IActionResult AddStudentData([FromQuery] string captcha, [FromBody]Student student)
         {
 
-            var login = db.Student.Where(p => p.Login == student.Login).Count();
-            var email = db.Student.Where(p => p.Email == student.Email).Count();
-            if (login != 0)
-                return Ok("This login is already taken");
+            Regex EmailRegex = new Regex(@"^[a-z][a-z0-9_-]*@[a-z0-9]*\.[a-z]{2,3}$");
+            Regex PhoneNumberRegex = new Regex(@"[0-9]");
 
-            if (email != 0)
-                return Ok("This email is already taken");
-
-
-            string secretKey = "6LfUQ2gUAAAAAJ-GJa5h0RG25-GQhVKqOV6qkJbN";
-            var client = new WebClient();
-            var result = client.DownloadString(string.Format("https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}", secretKey, captcha));
-            var obj = JObject.Parse(result);
-            var status = (bool)obj.SelectToken("success");
-            if (status)
-            {
-                db.Student.Add(student);
-                db.SaveChanges();
-                return Ok("Successfully registered");
-            }
+            if (!EmailRegex.IsMatch(student.Email))
+                return Ok("Invalid email");
+            else if (!PhoneNumberRegex.IsMatch(student.Phone))
+                return Ok("Invalid phone");
             else
-                return Ok("Google reCaptcha validation failed");
+            {
+                var login = db.Student.Where(p => p.Login == student.Login).Count();
+                var email = db.Student.Where(p => p.Email == student.Email).Count();
+                
+                if (login != 0)
+                    return Ok("This login is already taken");
 
+                if (email != 0)
+                    return Ok("This email is already taken");
+                string secretKey = "6LfUQ2gUAAAAAJ-GJa5h0RG25-GQhVKqOV6qkJbN";
+                var client = new WebClient();
+                var result = client.DownloadString(string.Format("https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}", secretKey, captcha));
+                var obj = JObject.Parse(result);
+                var status = (bool)obj.SelectToken("success");
+                if (status)
+                {
+                    db.Student.Add(student);
+                    db.SaveChanges();
+                    return Ok("Successfully registered");
+                }
+                else
+                    return Ok("Google reCaptcha validation failed");
+            }
            
            
 
