6c19aac6dce421750606321fa35aad0d85ad9f2f
kamil <kamil@DESKTOP-I3BD7EU>
2018-08-10 19:06:08 +0200

    v

--- Deanery/Controllers/CoursesController.cs
+++ Deanery/Controllers/CoursesController.cs
@@ -3,20 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Deanery.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Deanery.Controllers
 {
+
+
     public class CoursesController : Controller
     {
         private DbDeaneryContext db = new DbDeaneryContext();
 
         [Route("api/add/course")]
-        [HttpPost]
-        public IActionResult CreateNewCourse([FromBody]Course course)
+        [HttpGet]
+        public IActionResult CreateNewCourse()
         {
-           
 
+            Course course = new Course();
+            course.Description = "asddsa";
+            course.Difficulty = "hard";
+            course.Name = "dsad";
             try
             {
                 db.Course.Add(course);
@@ -28,6 +35,87 @@ namespace Deanery.Controllers
                 return Ok(ex.Message);
             }
 
+        }
+
+
+        [Route("api/add/studentcourse")]
+        [HttpGet]
+        public IActionResult AddStudentCourse([FromQuery]string NameCourse)
+        {
+            StudentCourse studentcourse = ExtractedStudentCourse(NameCourse);
+
+            try
+            {
+                db.StudentCourse.Add(studentcourse);
+                db.SaveChanges();
+                return Ok("Successfully added student to course");
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex.Message);
+            }
+
+        }
+
+
+        [Route("api/delete/studentcourse")]
+        [HttpGet]
+        public IActionResult RemoveStudentCourse([FromQuery]string NameCourse)
+        {
+            StudentCourse studentcourse = ExtractedStudentCourse(NameCourse);
+         
+            try
+            {
+                db.StudentCourse.Remove(studentcourse);
+                db.SaveChanges();
+                return Ok("Successfully removed student to course");
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex.Message);
+            }
+
+        }
+
+        private StudentCourse ExtractedStudentCourse(string NameCourse)
+        {
+
+            var student = HttpContext.Session.GetString("Login");
+            var queryStudent =
+                        from Onestudent in db.Student
+                        where Onestudent.Login == student
+                        select Onestudent;
+            var queryCourse =
+                        from onecourse in db.Course
+                        where onecourse.Name == NameCourse
+                        select onecourse;
+            
+
+            StudentCourse studentcourse = new StudentCourse();
+
+            foreach (Student Onestudent in queryStudent)
+            {
+                studentcourse.StudentId = Onestudent.Id;
+             
+
+            }
+            foreach (Course onecourse in queryCourse)
+            {
+                studentcourse.CourseId = onecourse.Id;
+
+            }
+            var queryStudentCourse =
+                        from oneStudentCourse in db.StudentCourse
+                        where oneStudentCourse.StudentId == studentcourse.StudentId && oneStudentCourse.CourseId == studentcourse.CourseId
+                        select oneStudentCourse;
+
+            foreach (StudentCourse onestudentcourse in queryStudentCourse)
+            {
+                studentcourse.Id = onestudentcourse.Id;
+
+            }
+
+            return studentcourse;
         }
     }
 }
