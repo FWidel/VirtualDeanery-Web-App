3f2ad1f35225f784173681b891fa1faf4007b416
FWidel <widelfranciszek@gmail.com>
2018-08-10 18:38:01 +0200

    Changed page view

--- Deanery/ClientApp/components/NavMenu.tsx
+++ Deanery/ClientApp/components/NavMenu.tsx
@@ -1,20 +1,48 @@
 import * as React from 'react';
 import { Link, NavLink } from 'react-router-dom';
+const serverUri = "api/"
+export class NavMenu extends React.Component<{}, { authorized: boolean }> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            authorized : false
+        }
+    }
 
-export class NavMenu extends React.Component<{}, {}> {
+
+    componentDidMount() {
+        var request = new XMLHttpRequest();
+    
+        request.open('POST', serverUri + 'user/check-authorization', true);
+        request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+
+        request.onload = () => {
+            if (request.responseText != "false") {
+
+                this.setState({
+                   authorized : true
+                });
+
+
+            }
+        }
+        request.send();
+  
+
+    }
     public render() {
-        return <div className='main-nav navMenu'>
+        return this.state.authorized == true ? < div className='main-nav navMenu' >
             <div className='navbar bg-light' >
                 <div className='navbar-header'>
                     <button type='button' className='navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse'>
                         <span className='sr-only'>Toggle navigation</span>
                     </button>
-                    <Link className='navbar-brand' to={ '/' }>Deanery</Link>
+                    <Link className='navbar-brand' to={'/'}>Deanery</Link>
                 </div>
                 <div className='navbar-collapse collapse'>
                     <ul className='nav'>
                         <li>
-                            <NavLink to={ '/' } exact activeClassName='active'>
+                            <NavLink to={'/index'} exact activeClassName='active'>
                                 <span className='glyphicon glyphicon-home'></span> Home
                             </NavLink>
                         </li>
@@ -24,14 +52,14 @@ export class NavMenu extends React.Component<{}, {}> {
                             </NavLink>
                         </li>
                         <li>
-                            <NavLink to={'/login'} exact activeClassName='active'>
-                                <span className='glyphicon glyphicon-home'></span> Login
+                            <NavLink to={'/courses'} exact activeClassName='active'>
+                                <span className='glyphicon glyphicon-home'></span> Courses
                             </NavLink>
                         </li>
- 
+
                     </ul>
                 </div>
             </div>
-        </div>;
+        </div> : <span> </span>;
     }
 }
