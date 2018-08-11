7575c94cde2238198028a611792fe7da88f7a085
FWidel <widelfranciszek@gmail.com>
2018-08-11 12:58:44 +0200

    Login view page mew

--- Deanery/ClientApp/components/Login.tsx
+++ Deanery/ClientApp/components/Login.tsx
@@ -2,21 +2,19 @@
 import { RouteComponentProps } from 'react-router';
 const serverUri = "/api/";
 import { Logout } from './Logout';
-export class Login extends React.Component<RouteComponentProps<User>, { Message : string}> {
+export class Login extends React.Component<RouteComponentProps<{}>, LoginStates>{
     constructor(props: any) {
         super(props);
         this.state = {
-            Message : ""
+            Message: ""
         }
-        this.userRegister = this.userRegister.bind(this);
+        this.userLogin = this.userLogin.bind(this);
     }
 
-    userRegister(z: any) {
+    userLogin(z: any) {
         z.preventDefault();
-        var formData;
-        formData = z.target;
-        console.log(formData);
 
+        var formData = z.target;
         var request = new XMLHttpRequest();
         var login = formData.login.value;
         var password = formData.password.value;
@@ -24,7 +22,6 @@ export class Login extends React.Component<RouteComponentProps<User>, { Message 
 
         request.open('POST', serverUri + 'user/login', true);
         request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-        //formData.reset();
         request.onload = () => {
             if (request.responseText != "Invalid Login or Password") {
                 window.location.replace("/index");
@@ -35,7 +32,6 @@ export class Login extends React.Component<RouteComponentProps<User>, { Message 
                 })
             }
         }
- 
 
         request.send(JSON.stringify({
             "Password": password,
@@ -44,17 +40,22 @@ export class Login extends React.Component<RouteComponentProps<User>, { Message 
         return false;
     }
 
+
+
     public render() {
         return <div>
-            <form className="form-group" onSubmit={this.userRegister} action="#">
+            <form className="form-group" onSubmit={this.userLogin}>
                 <div className="container login-form">
                     {this.state.Message != "" ? <div className="alert alert-danger">
                         <strong>Warning!</strong> {this.state.Message}
-                    </div> : <span/>}
+                    </div> : <span />}
+
                     <h3 className="text-center">Welcome</h3>
                     <p className="text-center">Please enter your login and password</p>
-                    <input type="text" placeholder="Login" className="form-control" id="login" name="login" required />
-                    <input type="password" placeholder="Password" className="form-control" id="password" name="password" required />
+                    <input type="text" placeholder="Login" className="form-control"
+                        id="login" name="login" autoComplete="true" required />
+                    <input type="password" placeholder="Password" className="form-control"
+                        id="password" name="password" autoComplete="true" required />
                     <button type="submit" className="btn btn-primary" >Log in</button>
                 </div>
             </form>
@@ -66,9 +67,7 @@ export class Login extends React.Component<RouteComponentProps<User>, { Message 
 }
 
 
-
-interface User {
-    Login: string,
-    Password: string
+interface LoginStates {
+    Message: string
 }
 
